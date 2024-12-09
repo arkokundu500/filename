68e39f7426e422292bd0cc33e3ea9dec8428024c
Arko Kundu <arkokundu500@gmail.com>
2024-12-09 20:09:55 +0530

    first commit

---
+++ p1.java
@@ -0,0 +1,23 @@
+public class PrimeNumberIdentification {
+
+    public static void main(String[] args) {
+        int number = 29; // You can change this number to test other values
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
